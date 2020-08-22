0a99d35023b41320f3b9a70c121a87bdb9ad8a16
Andres Benites <andres@benites.org>
2020-08-22 16:24:34 -0700

    Working for mouse clicks and single function at a time

--- java.js
+++ java.js
@@ -16,16 +16,60 @@ function divide(a, b) {
   return a / b;
 }
 
-function operate(funct, a, b) {
-  return funct(a, b);
+function operate(myFunct, a, b) {
+  return window[myFunct](a, b);
 }
-let regex = new RegExp(/\d|\.|\-|\+|\*|\/|\=/);
-let keys = document.addEventListener("keydown", (e) => {
-  digit = e.key;
-  if (!regex.test(digit)) return;
-  let pressedKey = document.getElementById(digit);
-  pressedKey.classList.add("pressed");
-});
+
+
+let totalValue = 0;
+let displayValue = 0;
+let funcPressed = "add";
+let myDisplay = document.querySelector("#display p");
+
+// // highlight calculator buttons when pushed with keyboard
+// let regex = new RegExp(/\d|\.|\-|\+|x|\/|\=\*/);
+// let keys = document.addEventListener("keydown", (e) => {
+//   digit = e.key;
+//   if (!regex.test(digit)) return;
+//   e.preventDefault();
+//   flashButton(e.key);
+//   updateDisplay(e.key);
+// });
+
+
+const buttons = document.getElementById("buttons");
+buttons.addEventListener("click",function(clicked) {
+    if (clicked.target.classList.contains("button")){
+        clicked.preventDefault();
+        clicked.target.classList.add("pressed");
+        if(clicked.target.classList.contains("clear")) {
+            clearDisplay();
+            clearVariables();
+            return;
+        }
+        if(clicked.target.classList.contains("equals")) {
+            displayValue = parseInt(myDisplay.textContent);
+            myDisplay.textContent = operate(funcPressed, totalValue, displayValue);
+            return;
+        }
+        if(clicked.target.classList.contains("func")){
+            funcPressed = clicked.target.id;
+            totalValue = parseInt(myDisplay.textContent);
+            clearDisplay();
+            return;
+        }
+        updateDisplay(clicked.target.textContent);
+    }
+})
+
+function clearDisplay(){
+    myDisplay.textContent = "0";
+}
+function clearVariables(){
+    totalValue = 0;
+    displayValue = 0;
+}
+// remove the animation when it's finished
 const allKeys = document.querySelectorAll(".button");
 allKeys.forEach((myButton) =>
   myButton.addEventListener("transitionend", (e) => {
@@ -33,7 +77,14 @@ allKeys.forEach((myButton) =>
   })
 );
 
-let totalValue = 0;
-display = document.querySelector("#display p");
-display.textContent = totalValue;
+
+function updateDisplay(value){
+    if (myDisplay.textContent == 0) myDisplay.textContent = "";
+    myDisplay.textContent += value;
+}
+
+function flashButton(buttonValue){
+    let pressedKey = document.getElementById(buttonValue);
+    pressedKey.classList.add("pressed");
+}
 
